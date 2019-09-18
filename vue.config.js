614a7c232f0ebd9e7d0f62c9c38f593b009669f0
renjimin <renjim@126.com>
2019-09-18 13:26:13 +0800

    修改打包文件

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   //  关闭eslint校验
   lintOnSave: false,
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/dist/' : '/'
+  // publicPath: process.env.NODE_ENV === 'production'
+  //   ? '/dist/' : '/'
+  publicPath: './'
+
 }
