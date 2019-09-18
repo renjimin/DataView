a05098aa2b708fa4b5021c49478a57a921999ed2
renjimin <renjim@126.com>
2019-09-18 13:35:02 +0800

    打包去掉.map相关文件

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   //  关闭eslint校验
   lintOnSave: false,
+  productionSourceMap: false,  // 生产环境的 source map
   // publicPath: process.env.NODE_ENV === 'production'
   //   ? '/dist/' : '/'
   publicPath: './'
